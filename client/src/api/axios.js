ec251d82926e61493bffe3196cf19e766aa9493a
Austin Houchens <austinhouchens1510@gmail.com>
2023-02-22 01:05:06 -0500

    set axios instance

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,5 +2,8 @@ import axios from "axios"
 const BASE_URL = "http://localhost:5000/api";
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    headers: { "Content-Type": "application/json",
+               "Accept": "application/json"
+    },
 })
