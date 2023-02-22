63d55cefaf6321c40213c3bb1b4e857bc80f1797
Austin Houchens <austinhouchens1510@gmail.com>
2023-02-22 01:00:18 -0500

    use allowedOrigins

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,8 +1,9 @@
-module.exports.corsOptions = {
-    origin: [
-        "http://localhost:5173", 
-        "http://127.0.0.1:5173"
-    ],
+const whitelistedOrigins = require("./whitelistedOrigins");
+
+const corsOptions = {
+    origin: whitelistedOrigins,
     credentials: true,
     optionsSuccessStatus: 200
 }
+
+module.exports = corsOptions;
