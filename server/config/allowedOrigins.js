dfadef4b4303e621282d067085f20e864e926b74
Austin Houchens <austinhouchens1510@gmail.com>
2023-02-22 01:03:43 -0500

    add allowed origins

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const whitelistedOrigins = ["http://localhost:5173", 
+                "http://localhost:5173/login",
+                "http://localhost:5173/register"
+]
+
+module.exports = whitelistedOrigins;
