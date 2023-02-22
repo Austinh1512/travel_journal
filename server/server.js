d91a052942ab0722d7fd55ca4246985a6fc59009
Austin Houchens <austinhouchens1510@gmail.com>
2023-02-22 00:56:27 -0500

    use setHeaders middleware and add auth router

--- server/server.js
+++ server/server.js
@@ -9,11 +9,13 @@ const corsOptions = require("./config/corsOptions");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/connect-db");
 const passport = require("passport");
+const setHeaders = require("./middleware/setHeaders");
 
 //Connect to Mongo
  connectDB();
 
 //Middleware
+app.use(setHeaders);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +27,9 @@ require("./config/passport-jwt");
 //Route Handlers
 const entryRouter = require("./routes/entries");
 app.use("/api/entries", entryRouter);
+
+const authRouter = require("./routes/auth");
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
     res.status(500).send(err.message);
