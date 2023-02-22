fa24d134569b1605eb451ea9db085b778444c5b4
Austin Houchens <austinhouchens1510@gmail.com>
2023-02-22 01:00:34 -0500

    configure passport jwt strategy

--- server/config/passport-jwt.js
+++ server/config/passport-jwt.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("../models/User");
+
 
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -8,5 +10,14 @@ const options = {
 }
 
 passport.use(new JwtStrategy(options, (jwt_payload, done) => {
-    console.log(jwt_payload);
+    User.findOne({ username: jwt_payload.username }, (err, user) => {
+        if (err) {
+            console.log(err);
+            return done(err, false);
+        }
+
+        if (user) {
+            return done(null, user);
+        }
+    })
 }))
