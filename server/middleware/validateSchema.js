cdf7c806058d9979e963b7748728be3ae7001071
Austin Houchens <austinhouchens1510@gmail.com>
2023-04-16 22:37:24 -0400

    change validation behavior based on request method

--- server/middleware/validateSchema.js
+++ server/middleware/validateSchema.js
@@ -2,7 +2,8 @@ const { entrySchema } = require("../schemas");
 
 const validateSchema = async (req, res, next) => {
     try {
-        await entrySchema.validateAsync(JSON.parse(req.body.values));
+        if (req.method === "POST") await entrySchema.validateAsync(JSON.parse(req.body.values));
+        else if (req.method === "PUT") await entrySchema.validateAsync(req.body.values);
         next();
     } catch(error) {
         res.status(400).json({"error": error.details.map(err => err.message).join(",")});
