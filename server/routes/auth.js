b6817eba1051e121fa1c8776c32875d714049a98
Austin Houchens <austinhouchens1510@gmail.com>
2023-04-16 22:33:57 -0400

    add async error handler to routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const handleAsync = require("express-async-handler");
 const { getUserInfo, handleRegister, handleLogin, handleRefresh, handleLogout } = require("../controllers/auth");
 
-router.post("/register", handleRegister);
+router.post("/register", handleAsync(handleRegister));
 
-router.post("/login", handleLogin);
+router.post("/login", handleAsync(handleLogin));
 
-router.get("/refresh", handleRefresh);
+router.get("/refresh", handleAsync(handleRefresh));
 
-router.get("/logout", handleLogout);
+router.get("/logout", handleAsync(handleLogout));
 
-router.get("/:id", getUserInfo);
+router.get("/:id", handleAsync(getUserInfo));
 
 module.exports = router;
