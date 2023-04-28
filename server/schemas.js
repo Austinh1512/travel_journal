5865050b2ad45611759eb077b156115a828412d2
Austin Houchens <austinhouchens1510@gmail.com>
2023-04-27 20:04:17 -0400

    add custom joi extension to sanitize html

--- server/schemas.js
+++ server/schemas.js
@@ -1,10 +1,37 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+    type: "string",
+    base: joi.string(),
+    messages: {
+        "string.sanitize": "{{#label}} can not include HTML"
+    },
+    rules: {
+        sanitize: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {}
+                })
+                return ((clean !== value) ? helpers.error("string.sanitize", { value }) : clean);
+            }
+        }
+    }
+})
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.entrySchema = Joi.object({
-    place: Joi.string().required(),
-    country: Joi.string().required(),
+    place: Joi.string().required().sanitize(),
+    country: Joi.string().required().sanitize(),
     startDate: Joi.date().required(),
     endDate: Joi.date().greater(Joi.ref("startDate")).required(),
-    description: Joi.string().required(),
+    description: Joi.string().required().sanitize(),
     images: Joi.array().required()
+})
+
+module.exports.userSchema = Joi.object({
+    username: Joi.string().required().sanitize(),
+    password: Joi.string().required().sanitize()
 })
