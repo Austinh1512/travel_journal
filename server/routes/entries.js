3c3a13420df7513459129672f03f722aa37e3fd4
Austin Houchens <austinhouchens1510@gmail.com>
2023-04-27 20:03:10 -0400

    refactor to use validateEntrySchema

--- server/routes/entries.js
+++ server/routes/entries.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const handleAsync = require("express-async-handler");
 const { getEntries, addEntry, editEntry, deleteEntry } = require("../controllers/entries");
 const passport = require("passport");
-const validateSchema = require("../middleware/validateSchema");
+const validateEntrySchema = require("../middleware/validateEntrySchema");
 const multer = require("multer");
 const { storage } = require("../config/cloudinary");
 const upload = multer({ storage });
 
 
 router.route("/")
-    .post(passport.authenticate("jwt", { session: false }), upload.any(), validateSchema, handleAsync(addEntry))
+    .post(passport.authenticate("jwt", { session: false }), upload.any(), validateEntrySchema, handleAsync(addEntry))
 
 router.route("/:userID").get(handleAsync(getEntries));
     
 router.route("/:id")
-    .put(passport.authenticate("jwt", { session: false }), validateSchema, handleAsync(editEntry))
+    .put(passport.authenticate("jwt", { session: false }), validateEntrySchema, handleAsync(editEntry))
     .delete(passport.authenticate("jwt", { session: false }), handleAsync(deleteEntry))
 
 module.exports = router;
