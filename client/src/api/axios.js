2465fcf13503dbb4edb953aced070c89c0c9a94e
Austin Houchens <austinhouchens1510@gmail.com>
2023-04-29 06:28:45 -0400

    fix bug in production url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = (import.meta.env.DEV) ? "http://localhost:5000/api" : "https://travel-journal-api.onrender.com";
+const BASE_URL = (import.meta.env.DEV) ? "http://localhost:5000/api" : "https://travel-journal-api.onrender.com/api";
 
 export default axios.create({
     baseURL: BASE_URL,
