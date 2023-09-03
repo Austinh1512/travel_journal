8d6de7fb2a228c01e231234719f1af8152e50565
Austin Houchens <austinhouchens1510@gmail.com>
2023-09-03 14:11:33 -0400

    fix imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import Layout from "./pages/Layout";
 import PersistLogin from "./components/PersistLogin";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
-import LoginForm from "./pages/LoginForm";
-import RegisterForm from "./pages/RegisterForm";
+import LoginForm from "./pages/authentication/LoginForm";
+import RegisterForm from "./pages/authentication/RegisterForm";
 import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
