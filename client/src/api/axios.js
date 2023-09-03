db2f85a61fa3892c9e85625486d3168e640c93e6
Austin Houchens <austinhouchens1510@gmail.com>
2023-09-03 14:06:46 -0400

    refactor to use useEnvironment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.DEV
-  ? "http://localhost:5000/api"
-  : "https://travel-journal-api.onrender.com/api";
+import useEnvironment from "../hooks/useEnvironment";
+
+const BASE_URL = useEnvironment();
 
 export default axios.create({
   baseURL: BASE_URL,
